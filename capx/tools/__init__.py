0f01d771bf6842d58d8a1d5f5a642896358abd1f
沈昱帆 <2337763816@qq.com>
2026-07-03 11:12:52 +0800

    Add tool planner abstraction

--- capx/tools/__init__.py
+++ capx/tools/__init__.py
@@ -1,4 +1,5 @@
 from capx.tools.executor import ToolExecutor
+from capx.tools.planner import LlmToolPlanner, ScriptedToolPlanner
 from capx.tools.prompts import build_tool_planner_prompt, parse_tool_call_response
 from capx.tools.registry import ToolRegistry, build_registry_from_apis
 from capx.tools.schema import StepFeedback, ToolCall, ToolResult, ToolSpec
@@ -6,8 +7,10 @@ from capx.tools.state import ToolState
 from capx.tools.verifiers import StepVerifier
 
 __all__ = [
+    "LlmToolPlanner",
     "StepFeedback",
     "StepVerifier",
+    "ScriptedToolPlanner",
     "ToolExecutor",
     "ToolCall",
     "ToolRegistry",
