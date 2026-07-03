9e0f78f26f25dfc67d5278ebe792f3eaa76d92c8
沈昱帆 <2337763816@qq.com>
2026-07-03 11:07:26 +0800

    Add structured tool executor

--- capx/tools/__init__.py
+++ capx/tools/__init__.py
@@ -1,9 +1,11 @@
+from capx.tools.executor import ToolExecutor
 from capx.tools.registry import ToolRegistry, build_registry_from_apis
 from capx.tools.schema import StepFeedback, ToolCall, ToolResult, ToolSpec
 from capx.tools.state import ToolState
 
 __all__ = [
     "StepFeedback",
+    "ToolExecutor",
     "ToolCall",
     "ToolRegistry",
     "ToolResult",
