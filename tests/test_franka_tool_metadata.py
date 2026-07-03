9efb0871f5b26e60d4562b1fd2bc24441da8a335
沈昱帆 <2337763816@qq.com>
2026-07-03 19:53:25 +0800

    Remove primitive toolification path

--- tests/test_franka_tool_metadata.py
+++
@@ -1,8 +0,0 @@
-from capx.tools.franka_metadata import FRANKA_TOOL_METADATA
-
-
-def test_franka_tool_metadata_marks_core_tools():
-    assert FRANKA_TOOL_METADATA["segment_sam3_text_prompt"]["tags"] == ["perception"]
-    assert "low_confidence_mask" in FRANKA_TOOL_METADATA["segment_sam3_text_prompt"]["failure_modes"]
-    assert FRANKA_TOOL_METADATA["solve_ik"]["tags"] == ["planning"]
-    assert FRANKA_TOOL_METADATA["move_to_joints"]["tags"] == ["execution"]
