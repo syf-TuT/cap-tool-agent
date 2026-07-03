dd5312d5c7e5de78bfb64e352e3a0cd6bea0b07a
沈昱帆 <2337763816@qq.com>
2026-07-03 11:08:39 +0800

    Add step-level tool verifier

--- capx/tools/__init__.py
+++ capx/tools/__init__.py
@@ -2,9 +2,11 @@ from capx.tools.executor import ToolExecutor
 from capx.tools.registry import ToolRegistry, build_registry_from_apis
 from capx.tools.schema import StepFeedback, ToolCall, ToolResult, ToolSpec
 from capx.tools.state import ToolState
+from capx.tools.verifiers import StepVerifier
 
 __all__ = [
     "StepFeedback",
+    "StepVerifier",
     "ToolExecutor",
     "ToolCall",
     "ToolRegistry",
